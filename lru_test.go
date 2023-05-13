fa9d591ecc7fe0c60634cdb413f34951981e93fd
halalala222 <1741196223@qq.com>
2023-05-13 23:22:49 +0800

    feat:add testCase data

--- lru_test.go
+++ lru_test.go
@@ -1,6 +1,42 @@
 package lru
 
-import "testing"
+import (
+	"testing"
+)
+
+type kv struct {
+	key         string
+	value       any
+	expectValue any
+}
+
+var testCaseData = []kv{
+	{
+		key:         "1",
+		value:       1,
+		expectValue: 1,
+	},
+	{
+		key:         "2",
+		value:       2,
+		expectValue: 2,
+	},
+	{
+		key:         "3",
+		value:       3,
+		expectValue: 3,
+	},
+	{
+		key:         "4",
+		value:       4,
+		expectValue: 4,
+	},
+	{
+		key:         "5",
+		value:       5,
+		expectValue: 5,
+	},
+}
 
 func TestLinkNodeLast(t *testing.T) {
 	lruCache := InitLRU()
